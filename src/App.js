c56a428b561c572565d2cb494799738aa791cfc0
Athos Matos <ladiesman217.as@gmail.com>
2021-08-04 13:56:19 -0300

    updates in the button

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import {View,Text,StyleSheet} from 'react-native'
 import Primeiro from './components/Primeiro'
 import Component, {Comp1,Comp2} from "./components/multi"
 import MinMax from "./components/minmax"
+import Botao from "./components/botao"
 
 function App()
 {
     console.log('teste')
-    
+    //
     return (
         <View style={style.App}>
+            <Botao/>
             <MinMax minimu={3} maximu={20} />
             <Component/>
             <Comp1/>
